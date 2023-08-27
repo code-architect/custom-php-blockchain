e45c50bae0d5f43ec1ce06c76b1b529acdc0f61d
tas-indranil <codearchitectdeveloper@gmail.com>
2023-08-27 08:23:45 +0530

    created a bit coin address, leveraged python's EllepticCurve library and createdan api using flask framework.

---
+++ Blockchain/Python_Package/test.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify
+import secrets
+from EllepticCurve.EllepticCurve import Sha256Point
+from util import hash160, hash256
+
+app = Flask(__name__)
+
+
+@app.route('/generate_keys', methods=['GET'])
+def generate_keys():
+    Gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
+    Gy = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+
+    G = Sha256Point(Gx, Gy)
+
+    privateKey = secrets.randbits(256)
+
+    unCompressedPublicKey = privateKey * G
+    xpoint = unCompressedPublicKey.x
+    ypoint = unCompressedPublicKey.y
+
+    if ypoint.num % 2 == 0:
+        compressesKey = b"\x02" + xpoint.num.to_bytes(32, "big")
+    else:
+        compressesKey = b'\x03' + xpoint.num.to_bytes(32, 'big')
+
+    hsh160 = hash160(compressesKey)
+
+    main_prefix = b'\x00'
+
+    newAddr = main_prefix + hsh160
+
+    """Checksum"""
+    checksum = hash256(newAddr)[:4]
+    newAddr = newAddr + checksum
+    BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+    count = 0
+    for c in newAddr:
+        if c == 0:
+            count += 1
+        else:
+            break
+
+    """convert address to numeric form"""
+    num = int.from_bytes(newAddr, 'big')
+    prefix = '1' * count
+
+    result = ''
+    while num > 0:
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+
+    PublicAddress = prefix + result
+    return {"privateKey": privateKey, "publicAddress": PublicAddress}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
